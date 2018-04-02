a848158a170f9b05f5088d829eae8cff5473cdac
Sumit Das <sumitdas9234@gmail.com>
2018-04-02 23:55:49 +0530

    Final Commit to Perceptron
    A Simple Model of Perceptron.

--- Perceptron/Point.js
+++ Perceptron/Point.js
@@ -2,7 +2,7 @@
 function Point(x, y) {
   this.x = x; //X coordinate
   this.y = y; //y coordinate
-
+  this.bias = 1;
   //Function to generate the label for each point
   if (x > y) {
     this.label = 1; //below thwe line y = x
@@ -16,24 +16,29 @@ Point.prototype.render = function() {
 
   //if the label is -ve, fill the circle with black
   if (this.label == -1) {
-    fill(0); //fill black
+    strokeWeight(1);
+    stroke(0); //fill black
+    noFill();
   } else {
-    fill(255); //fill white
+    noStroke();
+    fill(0); //fill white
   }
-  stroke(0);
-  ellipse(this.x, this.y, 15, 15); //draw the ellipse
+  ellipseMode(CENTER);
+  ellipse(this.x, this.y, 5, 5); //draw the ellipse
 };
 
 
 //Show the Green/Red Markers after the Perceptron guesses
 function putmarkers(guess, point) {
   //if the perceptron guesses correctly, display the point with a green dot
-  noStroke();
+  noFill();
+  strokeWeight(2);
   if (guess == point.label) {
-    fill(0, 255, 0); //fill green
-    ellipse(point.x, point.y, 10, 10); //draw a dot on the circle
+    stroke(color(76, 209, 55)); //fill green
+    ellipse(point.x, point.y, 15, 15); //draw a dot on the circle
   } else {
-    fill(255, 0, 0); //fill red
-    ellipse(point.x, point.y, 10, 10); //draw a dot on the circle
+    stroke(255, 0, 0); //fill red
+    ellipse(point.x, point.y, 15, 15); //draw a dot on the circle
   }
+  strokeWeight(1);
 }
